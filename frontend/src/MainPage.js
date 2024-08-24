e64eab9a73419f066db4e06619efcba8c09f6a5f
faye <nicolefayewong@gmail.com>
2024-08-24 14:17:29 +0800

    frontend

--- frontend/src/MainPage.js
+++ frontend/src/MainPage.js
@@ -5,6 +5,8 @@ import Header from './components/Header';
 import Features from './components/Features';
 import About from './components/About';
 import FAQ from './components/FAQ';
+import Login from './Login';
+import ImageCaptioning from './childUser/ImageCaptioning';
 import Footer from './components/Footer'; // You can include the Footer here if it should be part of this page
 
 const MainPage = () => {
@@ -15,6 +17,8 @@ const MainPage = () => {
       <div id="features"><Features /></div>
       <div id="about"><About /></div>
       <div id="faq"><FAQ /></div>
+      <div id="login"><Login /></div>
+      <div id="imagecaptioning"><ImageCaptioning /></div>
       <Footer />
     </>
   );
