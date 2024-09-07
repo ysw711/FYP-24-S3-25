ab7b50cfad89aa073ccf63215d79b8db92ff4cca
kaijieee <76198882+kaijieee@users.noreply.github.com>
2024-09-08 01:34:35 +0800

    updated navbar

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -1,12 +1,34 @@
 import React, { useState } from 'react';
 import { SiDatabricks } from 'react-icons/si';
 import { FaBars, FaTimes } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
     const handleNav = () => setNav(!nav);
+
+    const email = localStorage.getItem('email');
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        const response = await fetch('http://localhost:5000/logout', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            localStorage.removeItem('email');
+            // alert(data.message);
+            navigate('/login');
+        } else {
+            alert("Logout failed. Please try again.");
+        }
+    };
 
     return (
         <div className='navbar'>
@@ -20,7 +42,14 @@ const Navbar = () => {
                     <li><Link to='/ourApp'>Our App</Link></li>
                     <li><Link to='/about'>About</Link></li>
                     <li><Link to='/contact'>Contact</Link></li>
-                    <li><Link to='/login'>Login</Link></li>
+                    {/* <li><Link to='/login'>Login</Link></li> */}
+                    {email ? (
+                        <>
+                            <li><Link to="/" onClick={handleLogout}>Logout</Link></li>
+                        </>
+                    ) : (
+                        <li><Link to='/login'>Login</Link></li>
+                    )}
                 </ul>
                 <div className='hamburger' onClick={handleNav}>
                     {!nav ? <FaBars className='icon' /> : <FaTimes className='icon' />}
