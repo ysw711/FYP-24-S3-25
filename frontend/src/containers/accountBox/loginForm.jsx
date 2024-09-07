9fefbfb6e1c8484ef424a760fe440269b4f81ff2
kaijieee <76198882+kaijieee@users.noreply.github.com>
2024-09-08 01:35:37 +0800

    updated sign up and login form

--- frontend/src/containers/accountBox/loginForm.jsx
+++ frontend/src/containers/accountBox/loginForm.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import {
   BoldLink,
   BoxContainer,
@@ -13,21 +13,46 @@ import { AccountContext } from "./accountContext";
 export function LoginForm(props) {
   const { switchToSignup } = useContext(AccountContext);
 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch('http://localhost:5000/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      // alert(data.message);
+      localStorage.setItem('email', email);
+      window.location.href = '/';
+    } else {
+      alert(data.message);
+    }
+  };
+
   return (
     <BoxContainer>
-      <FormContainer>
-        <Input type="email" placeholder="Email" />
-        <Input type="password" placeholder="Password" />
+      <FormContainer onSubmit={handleSubmit}>
+        <Input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+        <Input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
+        <Marginer direction="vertical" margin={10} />
+        <MutedLink href="#">Forget your password?</MutedLink>
+        <Marginer direction="vertical" margin="1.6em" />
+        <SubmitButton type="submit">Signin</SubmitButton>
       </FormContainer>
-      <Marginer direction="vertical" margin={10} />
-      <MutedLink href="#">Forget your password?</MutedLink>
-      <Marginer direction="vertical" margin="1.6em" />
-      <SubmitButton type="submit">Signin</SubmitButton>
       <Marginer direction="vertical" margin="1em" />
       <MutedLink href="#">
-        Don't have an accoun?{" "}
+        Don't have an account?{" "}
         <BoldLink href="#" onClick={switchToSignup}>
-          Signup
+          Sign up
         </BoldLink>
       </MutedLink>
     </BoxContainer>
--- frontend/src/containers/accountBox/signupForm.jsx
+++ frontend/src/containers/accountBox/signupForm.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import {
   BoldLink,
   BoxContainer,
@@ -13,21 +13,57 @@ import { AccountContext } from "./accountContext";
 export function SignupForm(props) {
   const { switchToSignin } = useContext(AccountContext);
 
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Check if password and confirm password match
+    if (password !== confirmPassword) {
+      alert("Passwords do not match!");
+      return;
+    }
+
+    const response = await fetch('http://localhost:5000/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        password
+      }),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      alert(data.message);
+      switchToSignin();
+    } else {
+      alert(data.message);
+    }
+  };
+
   return (
     <BoxContainer>
-      <FormContainer>
-        <Input type="text" placeholder="Full Name" />
-        <Input type="email" placeholder="Email" />
-        <Input type="password" placeholder="Password" />
-        <Input type="password" placeholder="Confirm Password" />
-      </FormContainer>
-      <Marginer direction="vertical" margin={10} />
-      <SubmitButton type="submit">Signup</SubmitButton>
+      <FormContainer onSubmit={handleSubmit}>
+        <Input type="text" placeholder="Full Name" value={name} onChange={(e) => setName(e.target.value)} required/>
+        <Input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+        <Input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
+        <Input type="password" placeholder="Confirm Password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} required/>
+        <Marginer direction="vertical" margin={20} />
+        <SubmitButton type="submit">Signup</SubmitButton>
+      </FormContainer>      
       <Marginer direction="vertical" margin="1em" />
       <MutedLink href="#">
         Already have an account?
         <BoldLink href="#" onClick={switchToSignin}>
-          Signin
+          Sign in
         </BoldLink>
       </MutedLink>
     </BoxContainer>
